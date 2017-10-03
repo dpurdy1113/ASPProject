703c8793163639bf3e504f643226e7fd555f90e8
Douglas Purdy <dpurdy@my.wgu.edu>
2017-10-03 07:00:41 -0400

    added DBcontext and other EF settings

--- ASPFinalProject/Startup.cs
+++ ASPFinalProject/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using ASPFinalProject.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace ASPFinalProject
 {
@@ -21,6 +23,8 @@ namespace ASPFinalProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<SchoolContext>(options =>
+        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc();
         }
 
