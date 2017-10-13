d7f0a38f8c9412939cb12a2775237fa31345ad47
Douglas Purdy <dpurdy@my.wgu.edu>
2017-10-13 08:53:22 -0400

    updated CRUD functions

--- ASPFinalProject/Controllers/StudentsController.cs
+++ ASPFinalProject/Controllers/StudentsController.cs
@@ -34,6 +34,9 @@ namespace ASPFinalProject.Controllers
             }
 
             var student = await _context.Students
+                .Include(s => s.Enrollments)
+                .ThenInclude(e => e.Course)
+                .AsNoTracking()
                 .SingleOrDefaultAsync(m => m.ID == id);
             if (student == null)
             {
@@ -54,13 +57,22 @@ namespace ASPFinalProject.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,LastName,FirstMidName,EnrollmentDate")] Student student)
+        public async Task<IActionResult> Create([Bind("LastName,FirstMidName,EnrollmentDate")] Student student)
         {
-            if (ModelState.IsValid)
+            try
             {
-                _context.Add(student);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                if (ModelState.IsValid)
+                {
+                    _context.Add(student);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch(DbUpdateException)
+            {
+                ModelState.AddModelError("", "Unable to save changes. " +
+                "Try again, and if the problem persists " +
+                "see your system administrator.");
             }
             return View(student);
         }
@@ -125,7 +137,7 @@ namespace ASPFinalProject.Controllers
             }
 
             var student = await _context.Students
-                .SingleOrDefaultAsync(m => m.ID == id);
+               .SingleOrDefaultAsync(m => m.ID == id);
             if (student == null)
             {
                 return NotFound();
